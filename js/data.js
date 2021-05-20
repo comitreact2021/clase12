58ee8649cbb1df7c07f6df7057643c68e2d803aa
Pablo Tilli <tillipablo@gmail.com>
2021-05-20 12:00:43 -0300

    Se agrego la funcionalidad de busqueda por nombre al pulsar el boton buscar

--- js/data.js
+++ js/data.js
@@ -1,26 +1,32 @@
 const productos = [
   {
-    nombre: 'Producto 1',
+    nombre: 'Samsung',
     descripcion: 'Descripcion del producto 1.',
     precio: '1000',
     imagen: 'img3.jpg',
   },
   {
-    nombre: 'Producto 2',
+    nombre: 'LG',
     descripcion: 'Descripcion del producto 2.',
     precio: '15000',
     imagen: 'img2.jpeg',
   },
   {
-    nombre: 'Producto 3',
+    nombre: 'Iphone',
     descripcion: 'Descripcion del producto 3.',
     precio: '9000',
     imagen: 'img1.webp',
   },
   {
-    nombre: 'Producto 4',
+    nombre: 'Huawei',
     descripcion: 'Descripcion del producto 4.',
     precio: '120000',
     imagen: 'img3.jpg',
+  },
+  {
+    nombre: 'Xiaomi',
+    descripcion: 'Descripcion del producto 5.',
+    precio: '110000',
+    imagen: 'img1.webp',
   },
 ];
--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-function agregarTarjeta(producto) {
+function agregarTarjeta({ imagen, nombre, precio }) {
+  //const { imagen, nombre, precio } = producto;
+
   const card = `<div class="col mb-4 mt-4">
                       <a class="card-link" href="http://www.google.com.ar">
                           <div class="card h-100">
-                          <img src="images/${producto.imagen}" class="card-img-top" alt="Producto 1" />
+                          <img src="images/${imagen}" class="card-img-top" alt="Producto 1" />
   
                           <div class="card-body">
-                              <h5 class="card-title">${producto.nombre}</h5>
+                              <h5 class="card-title">${nombre}</h5>
   
                               <p class="card-text">
                               Some quick example text to build on the card title and make up
@@ -14,7 +16,7 @@ function agregarTarjeta(producto) {
                           </div>
   
                           <div class="card-footer">
-                              <small class="text-muted">$ ${producto.precio}</small>
+                              <small class="text-muted">$ ${precio}</small>
                           </div>
                           </div>
                       </a>
@@ -26,9 +28,37 @@ function agregarTarjeta(producto) {
 }
 
 function cargarTarjetas() {
-  /* for (let producto of productos) {
-    agregarTarjeta(producto);
-  } */
+  productos.forEach(agregarTarjeta);
 }
 
 cargarTarjetas();
+
+function buscarPorNombre(nombreBuscado) {
+  const productosContainer = document.getElementById('productos-container');
+
+  productosContainer.innerHTML = ''; //Borrar el listado de productos
+
+  for (producto of productos) {
+    const productoEnMinusuculas = producto.nombre.toLowerCase(); //samsung
+    const productoBuscadoEnMinusculas = nombreBuscado.toLowerCase(); //sung
+
+    if (productoEnMinusuculas.indexOf(productoBuscadoEnMinusculas) >= 0) {
+      agregarTarjeta(producto);
+    }
+  }
+}
+
+const btnBuscar = document.getElementById('btn-buscar');
+btnBuscar.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const txtBuscar = document.getElementById('txt-buscar');
+  const textoBuscado = txtBuscar.value;
+
+  //if (textoBuscado.trim() != '') {
+  if (textoBuscado.trim().length > 0) {
+    buscarPorNombre(textoBuscado);
+  } else {
+    alert('Debe ingresar un texto a buscar');
+  }
+});
